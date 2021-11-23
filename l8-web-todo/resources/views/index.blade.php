94d1ca6c226f8aca1667c73d2bfff771cf59bce8
profiterol <erollooper@gmail.com>
2021-11-23 23:59:37 +0800

    implementing priorities

--- l8-web-todo/resources/views/index.blade.php
+++ l8-web-todo/resources/views/index.blade.php
@@ -2,7 +2,7 @@
     {{--Table--}}
     <x-table>
         @foreach($todolists as $list)
-            <x-list-item item="{{$list->title}}" href="{{route('show',$list->id)}}">
+            <x-list-item item="{{$list->title}}" href="{{route('show',$list->id)}}" priority="{{$list->priority}}">
                 <form action="{{route('destroy',$list->id)}}" method="POST">
                     @csrf
                     @method('delete')
